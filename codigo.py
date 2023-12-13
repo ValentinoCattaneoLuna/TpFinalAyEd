e5dfb0f661bba6b64cf3f4012308fd3429b44d3b
xoppus <131176387+xoppus@users.noreply.github.com>
2023-12-13 12:05:54 -0300

    DocStrings codigo.py

--- codigo.py
+++ codigo.py
@@ -1,6 +1,14 @@
 import funcionestp as fn
 
 def main() ->None:
+    """
+    Función principal que ejecuta el programa de procesamiento de ventas.
+    1. Lee el archivo 'Productos_Elie.csv'.
+    2. Muestra un menú con opciones al usuario.
+    3. Permite al usuario procesar una venta, cargando productos y generando un ticket de compra.
+    4. Permite al usuario cerrar el programa.
+    El programa solo se ejecuta si no hay errores al leer el archivo. En caso contrario, se muestra un error.
+    """
     arch=fn.leer_archivo()
     if arch:
         encabezado,datos = fn.parsear(arch)
